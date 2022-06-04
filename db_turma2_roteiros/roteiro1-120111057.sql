c089626cd1add00b946a20aaff18de1447d8979a
João Vitor Mota <joao.mota@ccc.ufcg.edu.br>
2022-06-04 15:35:10 -0300

    Criado nova pasta de arquivos relacionada à Banco de Dados

---
+++ db_turma2_roteiros/roteiro1-120111057.sql
@@ -0,0 +1,37 @@
+-- QUESTÃO 1
+CREATE TABLE automovel (
+    veiculo VARCHAR(10), -- Carro/Caminhão/Moto
+    marca VARCHAR(20), -- Fiat/Renault
+    modelo VARCHAR(20),  -- Uno/EcoSport 
+    ano CHAR(4),
+    placa CHAR(7),
+    cpf_proprietario CHAR(11) 
+);
+
+CREATE TABLE segurado (
+    
+);
+
+CREATE TABLE perito (
+
+);
+
+CREATE TABLE oficina (
+
+);
+
+CREATE TABLE seguro (
+
+);
+
+CREATE TABLE sinistro (
+
+);
+
+CREATE TABLE pericia (
+
+);
+
+CREATE TABLE reparo (
+
+);
