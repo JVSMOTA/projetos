20ee3ad02d8a4db275f226c3fbc5ff3cf22ecc94
João Vitor Mota <joao.mota@ccc.ufcg.edu.br>
2022-06-04 19:41:31 -0300

    Mudanças no roteiro 1

--- db_turma2_roteiros/roteiro1-120111057.sql
+++ db_turma2_roteiros/roteiro1-120111057.sql
@@ -5,33 +5,54 @@ CREATE TABLE automovel (
     modelo VARCHAR(20),  -- Uno/EcoSport 
     ano CHAR(4),
     placa CHAR(7),
-    cpf_proprietario CHAR(11) 
+    cpf_proprietario CHAR(11),
+    id_veiculo SERIAL
 );
 
 CREATE TABLE segurado (
-    
+    cpf CHAR(11),
+    nome VARCHAR(40),
+    placa_veiculo CHAR(7),
+    id_veiculo SERIAL
 );
 
 CREATE TABLE perito (
-
+    id_perito SERIAL,
+    nome VARCHAR(40),
+    cpf CHAR(11)
 );
 
 CREATE TABLE oficina (
-
+    nome VARCHAR(40),
+    localidade VARCHAR(40),
+    id_oficina SERIAL
 );
 
 CREATE TABLE seguro (
-
+    valor NUMERIC,
+    validade DATE,
+    cpf_comprador CHAR(11),
+    cpf_proprietario CHAR(11),
+    sinistro SERIAL
 );
 
 CREATE TABLE sinistro (
-
+    id_sinistro SERIAL
 );
 
 CREATE TABLE pericia (
-
+    reparo BOOLEAN,
+    perda_total BOOLEAN,
+    descricao TEXT
 );
 
 CREATE TABLE reparo (
-
+    id_veiculo SERIAL,
+    placa_veiculo CHAR(7)
 );
+
+
+
+
+
+
